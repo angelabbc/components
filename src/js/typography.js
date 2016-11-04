48affc3c227679782f4c81357bade9f3afeb80bd
Angela Luk <angela.luk90@gmail.com>
2016-11-04 21:55:32 +0000

    added templates for size table too

---
+++ src/js/typography.js
@@ -0,0 +1,67 @@
+var exampleText = "The quick brown fox jumps over the lazy dog.";
+
+var birdParameters = [{
+  typeSize: "Eagle",
+  typeSettings: {
+    g3: "46/54",
+    g2: '36/42',
+    g1: '32/36'
+  },
+  exampleText: exampleText
+}, {
+  typeSize: "Buzzard",
+  typeSettings: {
+    g3: "32/32",
+    g2: '24/28',
+    g1: '24/24'
+  },
+  exampleText: exampleText
+}, {
+  typeSize: "Heron",
+  typeSettings: {
+    g3: "24/28",
+    g2: '20/24',
+    g1: '20/24'
+  },
+  exampleText: exampleText
+}, {
+  typeSize: "Goose",
+  typeSettings: {
+    g3: "20/20",
+    g2: '20/24',
+    g1: '18/22'
+  },
+  exampleText: exampleText
+}, {
+  typeSize: "Skylark",
+  typeSettings: {
+    g3: "15/20",
+    g2: '16/22',
+    g1: '16/22'
+  },
+  exampleText: exampleText
+}, {
+  typeSize: "Canary",
+  typeSettings: {
+    g3: "15/20",
+    g2: '16/22',
+    g1: '16/22'
+  },
+  exampleText: exampleText
+}, {
+  typeSize: "Bullfinch",
+  typeSettings: {
+    g3: "13/16",
+    g2: '14/18',
+    g1: '13/16'
+  },
+  exampleText: exampleText
+}, {
+  typeSize: "Robin",
+  typeSettings: {
+    g3: "13/16",
+    g2: '11/16',
+    g1: '13/16'
+  },
+  exampleText: exampleText
+}];
