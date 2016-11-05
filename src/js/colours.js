fc4b18aa15424cc0efb133f7d4c173c3d0c58411
Angela Luk <angela.luk90@gmail.com>
2016-11-05 16:22:51 +0000

    populated primary and secondary palettes

--- src/js/colours.js
+++ src/js/colours.js
@@ -5,19 +5,59 @@ var primaryColours = [{
   hex:"#000000"
   },
   {
-    colourName: "White",
-    rgb:"255,255,2555",
+    colourName: "Gray",
+    rgb:"37,37,37",
+    hex:"#252525"
+  },
+  {
+    colourName: "white",
+    rgb:"255,255,255",
     hex:"#FFFFFF"
   },
   {
     colourName: "Magenta",
     rgb:"245,73,151",
-    hex:"#f54997"
-  },
-  {
-    colourName: "Gray",
-    rgb:"37,37,37",
-    hex:"#252525"
+    hex:"#F54997"
   }];
 
 // Secondary Colours
+
+var SecondaryColours = [{
+  colourName: "Night",
+  rgb:"30,30,30",
+  hex:"#1E1E1E"
+  },
+  {
+    colourName: "Charcoal",
+    rgb:"42,42,42",
+    hex:"#2A2A2A"
+  },
+  {
+    colourName: "Storm",
+    rgb:"51,51,51",
+    hex:"#333333"
+  },
+  {
+    colourName: "Stone",
+    rgb:"68,68,68",
+    hex:"#444444"
+  },
+  {
+    colourName: "Concrete",
+    rgb:"119,119,119",
+    hex:"#777777"
+  },{
+    colourName: "Silver",
+    rgb:"187,187,187",
+    hex:"#BBBBBB"
+  },
+  {
+    colourName: "Platinum",
+    rgb:"238,238,238",
+    hex:"#EEEEEE"
+  },
+  {
+    colourName: "Raspberry",
+    rgb:"207,62,128",
+    hex:"#CF3E80"
+  }];
