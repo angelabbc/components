f93bd4da2c2eb5227e4c1a9459f4fd0e04930a4b
angelabbc <luka02@mc-s083466.local>
2016-11-11 14:29:05 +0000

    added more icons

--- src/js/icons.js
+++ src/js/icons.js
@@ -1,7 +1,24 @@
 //iconParameters
 
-var iconParameters = [{
-  iconName: "previous",
-  icon: "gel-icon-previous",
-  iconParent:"core"
-  }];
+var AVIconSet = [{
+  iconName: "3D",
+  icon:"3d",
+  iconParent:"audio-visual"
+},
+{
+  iconName: "Audio Descrption",
+  icon:"audio-description",
+  iconParent:"audio-visual"
+},
+{
+  iconName: "CC",
+  icon:"closed-caption",
+  iconParent:"audio-visual"
+},
+{
+  iconName: "Contiuous Play",
+  icon:"continous-play",
+  iconParent:"audio-visual"
+},
+
+];
